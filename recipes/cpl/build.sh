500dec63f2b17d3c91373253282eb693445d8973
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2022-02-28 16:50:58 +0100

    Specify location of cfitsio/wcslib/fftw, have bash as build dependency

--- recipes/cpl/build.sh
+++ recipes/cpl/build.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # CPL's custom M4 macro that detects FFTW needs to find its shared library.
 # Putting the lib path directly in LD_LIBRARY_PATH does the trick,
 # and is actually recommended by the CPL documentation,
 # https://ftp.eso.org/pub/dfs/pipelines/libraries/cpl/docs/cpl-user-manual_6.0.pdf
 env LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}" \
-  ./configure --prefix=${PREFIX}
+  ./configure --prefix=${PREFIX} --with-cfitsio=${PREFIX} --with-wcslib=${PREFIX} --with-fftw=${PREFIX}
 make
 make install
 
